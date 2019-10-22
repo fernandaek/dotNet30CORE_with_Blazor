895e634800240c6d40be6f49b5c2f8b5a093898b
Fernanda Ek <fernandaek@icloud.com>
2019-10-22 10:47:14 +0200

    xUnit Test - Student Class

---
+++ Exercises.Tests/Chapter_03/StudentTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Exercises.Classes.Chapter_03;
+
+namespace Exercises.Tests.Chapter_03
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void CanCreateStudentDefaultInstance()
+        {
+            var student = new Student();
+
+            Assert.NotNull(student);
+        }
+
+        [Fact]
+        public void CanCreateStudentInstanceWithParameters()
+        {
+            var student = new Student("Fernanda", "Ek", 30);
+
+            Assert.NotNull(student);
+            Assert.Equal(30, student.age);
+            Assert.Equal("Fernanda Ek", student.FullName);
+        }
+    }
+}
